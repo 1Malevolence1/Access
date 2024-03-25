4a3edff1789381894097363a1b0af3751608e701
Mr.D <danil36rusdanil@mail.ru>
2024-03-25 10:39:29 +0300

    Первый комит

---
+++ src/main/java/com/example/TestAccess/TestAccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestAccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestAccess/TestAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestAccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
